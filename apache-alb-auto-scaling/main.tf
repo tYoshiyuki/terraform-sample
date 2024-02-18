c222ac095943a1cc3c1e86755d3ebf6e40396378
Yoshiyuki Tsunomoto <wasiwasimaru@gmail.com>
2024-02-18 12:04:41 +0900

    update 更新。

--- apache-alb-auto-scaling/main.tf
+++ apache-alb-auto-scaling/main.tf
@@ -1,6 +1,4 @@
-data "aws_caller_identity" "current" {}
-
-resource "aws_security_group" "security_group_alb" {
+resource "aws_security_group" "alb" {
   description = local.sg_alb_name
   name        = local.sg_alb_name
   tags = {
@@ -33,7 +31,7 @@ resource "aws_security_group" "security_group_alb" {
   }
 }
 
-resource "aws_security_group" "security_group_ec2" {
+resource "aws_security_group" "ec2" {
   description = local.sg_ec2_name
   name        = local.sg_ec2_name
   tags = {
@@ -66,10 +64,10 @@ resource "aws_security_group" "security_group_ec2" {
   }
 }
 
-resource "aws_autoscaling_group" "autoscaling_group" {
+resource "aws_autoscaling_group" "main" {
   name = local.auto_scaling_name
   launch_template {
-    id      = aws_launch_template.launch_template.id
+    id      = aws_launch_template.main.id
     version = "1"
   }
   min_size         = 1
@@ -82,7 +80,7 @@ resource "aws_autoscaling_group" "autoscaling_group" {
     var.vpc_subnet3
   ]
   target_group_arns = [
-    aws_lb_target_group.lb_target_group.arn
+    aws_lb_target_group.main.arn
   ]
   health_check_type         = "EC2"
   health_check_grace_period = 300
@@ -97,10 +95,10 @@ resource "aws_autoscaling_group" "autoscaling_group" {
   }
 }
 
-resource "aws_autoscaling_group" "autoscaling_group_canary" {
+resource "aws_autoscaling_group" "canary" {
   name = "${local.auto_scaling_name}-canary"
   launch_template {
-    id      = aws_launch_template.launch_template.id
+    id      = aws_launch_template.main.id
     version = "1"
   }
   min_size         = 1
@@ -113,7 +111,7 @@ resource "aws_autoscaling_group" "autoscaling_group_canary" {
     var.vpc_subnet3
   ]
   target_group_arns = [
-    aws_lb_target_group.lb_target_group_canary.arn
+    aws_lb_target_group.canary.arn
   ]
   health_check_type         = "EC2"
   health_check_grace_period = 300
@@ -128,21 +126,21 @@ resource "aws_autoscaling_group" "autoscaling_group_canary" {
   }
 }
 
-resource "aws_launch_template" "launch_template" {
+resource "aws_launch_template" "main" {
   name      = local.launch_template_name
   user_data = filebase64("user_data.sh")
   iam_instance_profile {
     arn = aws_iam_instance_profile.iam.arn
   }
   vpc_security_group_ids = [
-    aws_security_group.security_group_ec2.id
+    aws_security_group.ec2.id
   ]
   key_name      = aws_key_pair.key_pair.key_name
   image_id      = local.image_id
   instance_type = local.instance_type
 }
 
-resource "aws_lb" "lb" {
+resource "aws_lb" "main" {
   name               = local.alb_name
   internal           = false
   load_balancer_type = "application"
@@ -152,7 +150,7 @@ resource "aws_lb" "lb" {
     var.vpc_subnet3
   ]
   security_groups = [
-    aws_security_group.security_group_alb.id
+    aws_security_group.alb.id
   ]
   ip_address_type = "ipv4"
   access_logs {
@@ -166,26 +164,26 @@ resource "aws_lb" "lb" {
   enable_cross_zone_load_balancing = "true"
 }
 
-resource "aws_lb_listener" "lb_listener" {
-  load_balancer_arn = aws_lb.lb.arn
+resource "aws_lb_listener" "main" {
+  load_balancer_arn = aws_lb.main.arn
   port              = 80
   protocol          = "HTTP"
   default_action {
     type = "forward"
     forward {
       target_group {
-        arn    = aws_lb_target_group.lb_target_group.arn
+        arn    = aws_lb_target_group.main.arn
         weight = 2
       }
       target_group {
-        arn    = aws_lb_target_group.lb_target_group_canary.arn
+        arn    = aws_lb_target_group.canary.arn
         weight = 1
       }
     }
   }
 }
 
-resource "aws_lb_target_group" "lb_target_group" {
+resource "aws_lb_target_group" "main" {
   health_check {
     interval            = 5
     path                = "/"
@@ -204,7 +202,7 @@ resource "aws_lb_target_group" "lb_target_group" {
   name                 = local.tg_name
 }
 
-resource "aws_lb_target_group" "lb_target_group_canary" {
+resource "aws_lb_target_group" "canary" {
   health_check {
     interval            = 5
     path                = "/"
@@ -223,43 +221,43 @@ resource "aws_lb_target_group" "lb_target_group_canary" {
   name                 = "${local.tg_name}-canary"
 }
 
-resource "aws_s3_bucket" "s3_bucket" {
+resource "aws_s3_bucket" "main" {
   bucket        = local.s3_name
   force_destroy = true
 }
 
-resource "aws_s3_object" "s3_object" {
-  bucket = aws_s3_bucket.s3_bucket.id
+resource "aws_s3_object" "main" {
+  bucket = aws_s3_bucket.main.id
   key    = "SampleApp_Linux.zip"
   source = "${path.module}/SampleApp_Linux.zip"
 }
 
-resource "aws_s3_bucket_versioning" "s3_bucket_versioning" {
-  bucket = aws_s3_bucket.s3_bucket.id
+resource "aws_s3_bucket_versioning" "main" {
+  bucket = aws_s3_bucket.main.id
   versioning_configuration {
     status = "Enabled"
   }
 }
 
-resource "aws_s3_bucket_ownership_controls" "s3_bucket_ownership_controls" {
-  bucket = aws_s3_bucket.s3_bucket.id
+resource "aws_s3_bucket_ownership_controls" "main" {
+  bucket = aws_s3_bucket.main.id
 
   rule {
     object_ownership = "BucketOwnerEnforced"
   }
 }
 
-resource "aws_codedeploy_app" "code_deploy_app" {
+resource "aws_codedeploy_app" "main" {
   name             = local.code_deploy_app_name
   compute_platform = "Server"
 }
 
-resource "aws_codedeploy_deployment_group" "codedeploy_deployment_group" {
-  app_name               = aws_codedeploy_app.code_deploy_app.name
+resource "aws_codedeploy_deployment_group" "in_place" {
+  app_name               = aws_codedeploy_app.main.name
   deployment_group_name  = local.codedeploy_deployment_group_name
   deployment_config_name = "CodeDeployDefault.AllAtOnce"
   service_role_arn       = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/CodeDeployRole"
-  autoscaling_groups     = [aws_autoscaling_group.autoscaling_group.name]
+  autoscaling_groups     = [aws_autoscaling_group.main.name]
 
   deployment_style {
     deployment_type   = "IN_PLACE"
@@ -268,17 +266,17 @@ resource "aws_codedeploy_deployment_group" "codedeploy_deployment_group" {
 
   load_balancer_info {
     target_group_info {
-      name = aws_lb_target_group.lb_target_group.name
+      name = aws_lb_target_group.main.name
     }
   }
 }
 
-resource "aws_codedeploy_deployment_group" "codedeploy_deployment_group_bg" {
-  app_name               = aws_codedeploy_app.code_deploy_app.name
+resource "aws_codedeploy_deployment_group" "bg" {
+  app_name               = aws_codedeploy_app.main.name
   deployment_group_name  = local.codedeploy_deployment_group_bg_name
   deployment_config_name = "CodeDeployDefault.AllAtOnce"
   service_role_arn       = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/CodeDeployRole"
-  autoscaling_groups     = [aws_autoscaling_group.autoscaling_group.name]
+  autoscaling_groups     = [aws_autoscaling_group.main.name]
 
   deployment_style {
     deployment_type   = "BLUE_GREEN"
@@ -302,16 +300,16 @@ resource "aws_codedeploy_deployment_group" "codedeploy_deployment_group_bg" {
 
   load_balancer_info {
     target_group_info {
-      name = aws_lb_target_group.lb_target_group.name
+      name = aws_lb_target_group.main.name
     }
   }
 }
 
-resource "aws_codepipeline" "codepipeline" {
+resource "aws_codepipeline" "main" {
   name     = local.codepipeline_name
   role_arn = aws_iam_role.iam_codepipeline.arn
   artifact_store {
-    location = aws_s3_bucket.s3_bucket.bucket
+    location = aws_s3_bucket.main.bucket
     type     = "S3"
   }
   stage {
@@ -322,7 +320,7 @@ resource "aws_codepipeline" "codepipeline" {
       owner    = "AWS"
       configuration = {
         PollForSourceChanges = "true"
-        S3Bucket             = aws_s3_bucket.s3_bucket.bucket
+        S3Bucket             = aws_s3_bucket.main.bucket
         S3ObjectKey          = "SampleApp_Linux.zip"
       }
       provider = "S3"
@@ -340,8 +338,8 @@ resource "aws_codepipeline" "codepipeline" {
       category = "Deploy"
       owner    = "AWS"
       configuration = {
-        ApplicationName     = aws_codedeploy_app.code_deploy_app.name
-        DeploymentGroupName = aws_codedeploy_deployment_group.codedeploy_deployment_group_bg.deployment_group_name
+        ApplicationName     = aws_codedeploy_app.main.name
+        DeploymentGroupName = aws_codedeploy_deployment_group.bg.deployment_group_name
       }
       input_artifacts = [
         "SourceArtifact"
